64d9baff2af333e97ef3df6c4ef339b50baaa515
nijmanc2 <90317886+DeCeiman@users.noreply.github.com>
2021-09-15 22:46:38 +0200

    Bouncing Attachements added
    Made a "model" for the 3 attachments
    they all bounce and float

--- PGD-GAM-JAM-2/Assets/Ciarán/Scripts/FloatAnimation.cs
+++ PGD-GAM-JAM-2/Assets/Ciarán/Scripts/FloatAnimation.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FloatAnimation : MonoBehaviour
 {
+    float floatSpan = 2.0f;
+    float speed = 1.0f;
+
+    private float startY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        X = 2;
-    }
+        
+        Vector3 bobbingEffect = new Vector3(0, (float)(startY + Mathf.Sin(Time.time * speed) * floatSpan / 6.0), 0);
+        
+        this.transform.position = bobbingEffect;
+       
+    } 
 }
