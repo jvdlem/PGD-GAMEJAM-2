180c63e4c1c426397e1dc8c6c1db4ccbc1ba5644
JordyWolf <33620374+Jordywolf@users.noreply.github.com>
2021-09-17 16:33:50 +0200

    added the boss

---
+++ PGD-GAM-JAM-2/Assets/Scripts/EyeBossScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeBossScript : MonoBehaviour
+{
+    private BossScript parentBoss;
+    private GameObject player;
+    public new Animation animation;
+    public float lookSpeed = 3f;
+
+    public new Renderer renderer;
+
+    public bool EyeIsActive;
+
+    void Start()
+    {
+        parentBoss = transform.parent.GetComponent<BossScript>();
+        renderer = GetComponent<Renderer>();
+        animation = GetComponent<Animation>();
+        player = parentBoss.Player;
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - new Vector3(transform.position.x, transform.position.y - 90, transform.position.z)), lookSpeed * Time.deltaTime);
+
+        if (EyeIsActive)
+        {
+            renderer.material.color = Color.white;
+        }
+        else
+        {
+            renderer.material.color = Color.black;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (EyeIsActive)
+        {
+            if (collision.gameObject.tag == "Bullet")
+            {
+                parentBoss.BossHealth -= 5;
+                parentBoss.EyeHits++;
+                if (parentBoss.BossHealth <= 0)
+                {
+                    parentBoss.bossState = 5;
+                }
+            }
+        }
+    }
+}
