6947091281c5feffa9c15bc3ec073691d9aec651
hsuwandhy <79214164+hsuwandhy@users.noreply.github.com>
2021-09-20 19:30:00 +0200

    Player health(work in progress)

---
+++ PGD-GAM-JAM-2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    public UIHealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Set conditions for taking damage
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+}
