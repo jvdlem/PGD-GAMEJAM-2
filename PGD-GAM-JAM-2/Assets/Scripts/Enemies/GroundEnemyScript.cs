887634bf98694c9c587a6655763044a7b88df54d
JordyWolf <33620374+Jordywolf@users.noreply.github.com>
2021-10-07 16:24:49 +0200

    added pathfinding, AI vision, attack distance

--- PGD-GAM-JAM-2/Assets/Scripts/Enemies/GroundEnemyScript.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Enemies/GroundEnemyScript.cs
@@ -7,18 +7,43 @@ public class GroundEnemyScript : EnemyBaseScript
 {
     private NavMeshAgent navMeshAgent;
     public NavMeshSurface Ground;
+
+    private Vector3 targetPosition;
+    private float checkForPlayerDistance = 100;
+    
+    private float attackRange = 5;
+
     void Start()
     {
+        Ground.BuildNavMesh();
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-            Ground.BuildNavMesh();
-        //}
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, (Player.transform.position - transform.position), out hit, checkForPlayerDistance))
+        {
+            Color color = Color.white;
+            if(hit.transform == Player.transform)
+            {
+                color = Color.green;
+                targetPosition = Player.transform.position;
+            }
+            else
+            {
+                color = Color.red;
+            }
+            Debug.DrawRay(transform.position, Player.transform.position - transform.position, color);
 
-        navMeshAgent.destination = Player.transform.position;
+            float Distance = Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(targetPosition.x, 0, targetPosition.z));
+            if (Distance >= attackRange)navMeshAgent.destination = targetPosition;
+            else navMeshAgent.destination = transform.position;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(targetPosition, 1f);
     }
 }
