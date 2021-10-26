debedbb3e0d7862e53763b60ce876513a419873f
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2021-10-26 13:32:25 +0200

    gobline 3.0

--- PGD-GAM-JAM-2/Assets/Scripts/Enemies/Melee Goblin/EnemyMove.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Enemies/Melee Goblin/EnemyMove.cs
@@ -4,32 +4,35 @@ using UnityEngine;
 
 public class EnemyMove : GroundEnemyScript
 {
-    [SerializeField] GameObject player;
-
-    [SerializeField] int trackDistance = 20; //tracking distance of the goblin vs the player.
     [SerializeField] int rushDistance = 8;
 
     // Start is called before the first frame update
     public override void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Playboy");
+        base.Start();
+        Health = 10;
+        Tier = 1;
+        checkForPlayerDistance = 20;
+        WalkSpeed = 4;
+        RotateSpeed = 8;
+        AttackRange = 2;
     }
 
     // Update is called once per frame
     public override void Update()
     {
-        float dist = Vector3.Distance(player.transform.position, transform.position);
+        base.Update();
+        float dist = Vector3.Distance(Player.transform.position, transform.position);
         float step = WalkSpeed * Time.deltaTime; // calculate distance to move
 
-        if (dist < trackDistance)
+        if (dist <= checkForPlayerDistance && dist >= AttackRange)
         {
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
-            transform.LookAt(player.transform);
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, step);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Player.transform.position - new Vector3(transform.position.x, transform.position.y - 90, transform.position.z)), RotateSpeed * Time.deltaTime);
             if (dist < rushDistance)
             {
                 step = (WalkSpeed * 2) * Time.deltaTime;
-                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
-
+                transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, step);
             }
         }
         if (Health <= 0)
