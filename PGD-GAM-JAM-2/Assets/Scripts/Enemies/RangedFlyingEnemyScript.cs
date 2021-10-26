0388f0c5d23fdedce905d9e0e7af5129eb4cb112
ldebont <90321941+ldebont@users.noreply.github.com>
2021-10-26 17:31:22 +0200

    "FlyingEnemyScript" and inheriting classes developed.

--- PGD-GAM-JAM-2/Assets/Scripts/Enemies/RangedFlyingEnemyScript.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Enemies/RangedFlyingEnemyScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class RangedFlyingEnemyScript : FlyingEnemyScript
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] GameObject projectile; //Projectile which can be filled in
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        
+        base.Update();
+
+        TrackObject(targetObject); //Follow target
+        FireOn(targetObject); //Fire projectile at target
+
+        FlyTo(target, flyingSpeed); //Fly in direction of target
+    }
+
+    /// <summary>Launches a projectile at a target</summary>
+    private void FireOn(GameObject targetObject) 
+    {
+        Instantiate(projectile, transform.position + target, transform.rotation);
     }
 }
