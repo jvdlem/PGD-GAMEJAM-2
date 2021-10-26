0ace45f8d4d7f9cb484269c140d84fb16313e426
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2021-10-26 12:35:39 +0200

    goblin 2.0 update now with rush attack

--- PGD-GAM-JAM-2/Assets/Scripts/Enemies/Melee Goblin/EnemyMove.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Enemies/Melee Goblin/EnemyMove.cs
@@ -2,32 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMove : MonoBehaviour
+public class EnemyMove : GroundEnemyScript
 {
     [SerializeField] GameObject player;
 
-    float speed = 4; //speed of the goblin.
-    int trackDistance = 20; //tracking distance of the goblin vs the player.
-    private float health = 10;
+    [SerializeField] int trackDistance = 20; //tracking distance of the goblin vs the player.
+    [SerializeField] int rushDistance = 8;
 
     // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
         player = GameObject.FindGameObjectWithTag("Playboy");
     }
 
     // Update is called once per frame
-    void Update()
+    public override void Update()
     {
         float dist = Vector3.Distance(player.transform.position, transform.position);
-        float step = speed * Time.deltaTime; // calculate distance to move
+        float step = WalkSpeed * Time.deltaTime; // calculate distance to move
 
         if (dist < trackDistance)
         {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
             transform.LookAt(player.transform);
+            if (dist < rushDistance)
+            {
+                step = (WalkSpeed * 2) * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+
+            }
         }
-        if (health <= 0)
+        if (Health <= 0)
         {
             Destroy(this.gameObject);
         }
@@ -37,7 +42,7 @@ public class EnemyMove : MonoBehaviour
     {
         if (collision.gameObject.tag == "Projectile")
         {
-            health -= 1;
+            Health -= 1;
             Destroy(collision.gameObject);
         }
     }
