c842e67ecd1bb13e4930f24518b10197fed705ee
EslamiAA <90322106+EslamiAA@users.noreply.github.com>
2021-10-26 17:12:15 +0200

    fixed healthUi
    Fixed healthUic

--- PGD-GAM-JAM-2/Assets/WatchUi.cs
+++ PGD-GAM-JAM-2/Assets/WatchUi.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 public class WatchUi : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject Canvas;
+    public Animation WatchUiAnim;
     public Text healthText;
     private void Update()
     {
         healthText.text = "" + FindObjectOfType<PlayerHealthScript>().currentHealth;
+        if (this.transform.eulerAngles.z >= 90f && this.transform.eulerAngles.z >= 200f)
+        {
+            Canvas.SetActive(true);
+            WatchUiAnim.Play("WatchAnimOpen");
+            
+        }
+        else
+        { 
+            Canvas.SetActive(false);
+            WatchUiAnim.Play("WatchAnimClose");
+        }
+        
     }
 
 
