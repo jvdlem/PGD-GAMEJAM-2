03f5ac32968fe2632e4f22b0cf493a1d14447c95
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2021-11-25 13:22:22 +0100

    bugfix for player, a, fps ui and a smol

--- PGD-GAM-JAM-2/Assets/Scripts/Enemies/Melee Goblin/EnemyMove.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Enemies/Melee Goblin/EnemyMove.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class EnemyMove : GroundEnemyScript
 {
-    [SerializeField] int rushDistance = 8;
     float AttackTimer;
-    private bool playOnce;
-
 
     // Start is called before the first frame update
     public override void Start()
     {
         base.Start();
-        rushDistance = 8;
         Health = 10;
         Tier = 1;
         checkForPlayerDistance = 20;
@@ -25,33 +21,9 @@ public class EnemyMove : GroundEnemyScript
     // Update is called once per frame
     public override void Update()
     {
+        this.transform.LookAt(new Vector3(Player.transform.position.x, this.transform.position.y, Player.transform.position.z));
+
         base.Update();
-        float dist = Vector3.Distance(Player.transform.position, transform.position);
-        float step = WalkSpeed * Time.deltaTime; // calculate distance to move
-
-        if (dist <= checkForPlayerDistance && dist >= AttackRange)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Player.transform.position - new Vector3(transform.position.x, transform.position.y - 90, transform.position.z)), RotateSpeed * Time.deltaTime);
-
-            if (dist < rushDistance)
-            {
-                step = (WalkSpeed * 2) * Time.deltaTime;
-                transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, step);
-                if (playOnce)
-                {
-
-                    FMODUnity.RuntimeManager.PlayOneShot("event:/Enemy/Goblin/GoblinWindup", this.gameObject.transform.position);
-                    playOnce = false;
-                }
-            }
-            else
-            {
-                step = WalkSpeed * Time.deltaTime;
-                transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, step);
-                playOnce = true;
-            }
-        }
-
         if (Health <= 0)
         {
             FMODUnity.RuntimeManager.PlayOneShot("event:/Enemy/Goblin/GoblinDeath", this.gameObject.transform.position);
@@ -60,7 +32,7 @@ public class EnemyMove : GroundEnemyScript
         Debug.Log(WalkSpeed);
     }
 
-    private void OnTriggerEnter(Collider collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Projectile")
         {
