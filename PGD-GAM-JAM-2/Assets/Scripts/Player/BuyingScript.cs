11f774e8ccdef128e1538b5c3d99382373bbad9f
nijmanc2 <90317886+DeCeiman@users.noreply.github.com>
2021-12-23 16:32:18 +0100

    start footsteps

--- PGD-GAM-JAM-2/Assets/Scripts/Player/BuyingScript.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Player/BuyingScript.cs
@@ -5,7 +5,6 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class BuyingScript : MonoBehaviour
 {
-    [SerializeField] DisplayItems displayItems;
     [SerializeField] PlayerHealthScript playerHealthScript;
     LayerMask shopButtons;
     private void Start()
@@ -17,15 +16,10 @@ public class BuyingScript : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit, 30f, shopButtons))
         {
-            if (Input.GetKeyDown(KeyCode.E) && playerHealthScript.coins >= displayItems.price)
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 hit.rigidbody.velocity += Vector3.down * 100;
             }
         }
-    }
-
-    public void BuyItemsVR()
-    {
-        playerHealthScript.coins -= displayItems.price;
     }
 }
