e5a9ea1d49dcd6acb88777b51e78f4b2637f021a
nijmanc2 <90317886+DeCeiman@users.noreply.github.com>
2022-01-12 11:31:51 +0100

    Updated FMOD and restarted buttons changes

--- PGD-GAM-JAM-2/Assets/Scripts/Player/PushButtons.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Player/PushButtons.cs
@@ -6,15 +6,15 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class PushButtons : MonoBehaviour
 {
     [SerializeField] PlayerHealthScript playerHealthScript;
-    LayerMask shopButtons;
+    LayerMask buttons;
     private void Start()
     {
-        shopButtons = LayerMask.GetMask("ShopButtons");
+        buttons = LayerMask.GetMask("Buttons");
     }
     private void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, 30f, shopButtons))
+        if (Physics.Raycast(ray, out RaycastHit hit, 10f, buttons))
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
