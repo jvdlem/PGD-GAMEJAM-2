bf835654a737178fa52771024297852ab9eaa774
hsuwandhy <hsuwandhy@gmail.com>
2022-01-14 13:07:04 +0100

    merge

--- PGD-GAM-JAM-2/Assets/ArrowScript.cs
+++ PGD-GAM-JAM-2/Assets/ArrowScript.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class ArrowScript : MonoBehaviour
 {
-    //[SerializeField] GameObject Skeleton;
-   // [SerializeField] Transform shootPoint;
-  //  GameObject player;
+    [SerializeField] GameObject Skeleton;
+    private GameObject shootPoint;
+    //  GameObject player;
     PlayerHealthScript playerHealth;
+    Vector3 direction;
     //Arrow damage multiplier from Archer script
     private void Start()
     {
+        shootPoint = GameObject.FindGameObjectWithTag("ArrowSpawnPoint");
+        direction = Skeleton.transform.position - shootPoint.transform.position;
+
+
         //this.transform.rotation = Quaternion.
         //transform.LookAt(Player);
         playerHealth = GetComponent<PlayerHealthScript>();
