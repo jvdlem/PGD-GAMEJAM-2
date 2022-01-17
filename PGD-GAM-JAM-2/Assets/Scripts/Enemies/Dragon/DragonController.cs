3d4d5c36c8974e6658306d443c28777328162aa1
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2022-01-18 00:56:38 +0100

    merge

--- PGD-GAM-JAM-2/Assets/Scripts/Enemies/Dragon/DragonController.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Enemies/Dragon/DragonController.cs
@@ -6,16 +6,21 @@ public class DragonController : MeleeFlyingEnemyScript
     [SerializeField]
     public Animator anim;
 
+    private bool attackTrigger;
+    private bool deathTrigger;
+
+    private bool coinDropped;
+
     public override void Start()
     { 
         base.Start();
 
         Tier = 1;
 
-        Health = 100;
+        Health = -1;
         Damage = 10;
 
-        currentState = States.Patrolling; //Enemy starts chasing
+        currentState = States.Chasing; //Enemy starts chasing
     }
 
     private void PlayAnimation(string animation)
@@ -23,14 +28,46 @@ public class DragonController : MeleeFlyingEnemyScript
         anim.SetTrigger(animation);
     }
 
+    public virtual void PlaySound(string soundPath, Vector3 position)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(soundPath, position);
+    }
+
     public override void Update()
     {
         base.Update();
 
-        if (currentState == States.Attacking) { PlayAnimation("Attack"); }
+        if (currentState == States.Attacking) 
+        {          
+            PlayAnimation("Attack");
+            attackTrigger = true;
+        }
         else { PlayAnimation("Idle"); }
 
-        if (Health < 0) PlayAnimation("Die");
+        if (Health <= 0) 
+        {
+            PlayAnimation("Die");
+
+            if (!coinDropped) 
+            {
+                Instantiate(Coin, transform.position, transform.rotation);
+                coinDropped = true;
+            }
+
+            deathTrigger = true;
+        }
+
+        if (attackTrigger) 
+        {
+            PlaySound("event:/Enemy/Bat/BatAttack", transform.position);
+            attackTrigger = false;
+        }
+
+        if (deathTrigger)
+        {
+            PlaySound("event:/Enemy/Bat/BatDeath", transform.position);
+            deathTrigger = false;
+        }
     }
 
     protected void OnTriggerEnter(Collider other)
@@ -42,5 +79,8 @@ public class DragonController : MeleeFlyingEnemyScript
 
         //If collision with player, damage player health
         if (other.gameObject.tag == "Player") { Player.GetComponent<PlayerHealthScript>().takeDamage(3); }
+
+        //Lose health if hit by projectile
+        if (other.gameObject.tag == "Projectile") { TakeDamage(1); }
     }
 }
