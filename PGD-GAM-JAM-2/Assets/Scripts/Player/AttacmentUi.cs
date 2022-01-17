09c4b3438e3740ba4fddb1662ba6847173726b49
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2022-01-18 00:01:10 +0100

    merge with main

--- PGD-GAM-JAM-2/Assets/Scripts/Player/AttacmentUi.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Player/AttacmentUi.cs
@@ -1,28 +1,121 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AttacmentUi : MonoBehaviour
 {
-    public GameObject attachmentUILayer;
+
     private float minDistanceFromAttachment = 5f;
     LayerMask attachments;
+    int AmountHit = 0;
+    [SerializeField] public Canvas attachmentStatsUi;
+    public int currentState;
+    public int set;
+    public List<float> statList;
+    public Text[] TstatList;
+    [SerializeField] int amountofRays;
+    [Header("CurrentAttachment")]
+    [SerializeField] GameObject currentobject;
+    [SerializeField] Image sprite;
+    [Header("Stats")]
+    [SerializeField] float spread;
+    [SerializeField] float amountOfBullets;
+    [SerializeField] float damage;
+    [SerializeField] float range;
+    [SerializeField] Text setText;
+
+
+
     private void Start()
     {
         attachments = LayerMask.GetMask("Interactible");
+
+        currentState = default;
+
     }
     private void Update()
     {
-        
-        RaycastHit hit;
-        Ray ray = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
-        
-        if (Physics.Raycast(ray, out hit, minDistanceFromAttachment, attachments))
+
+        Vector3 pos = transform.position;
+        AmountHit = 0;
+
+        state();
+
+        for (int x = -1; x <= 1; x++)
         {
-           
-            hit.rigidbody.gameObject.GetComponent<AttachmentStatsUi>().currentState = 1;
-            
+            for (int y = -1; y <= 1; y++)
+            {
+                Ray ray = new Ray(pos + new Vector3(x * 0.1f, y * 0.07f, 0), transform.TransformDirection(Vector3.forward));
+                RaycastHit hit;
+                Debug.DrawRay(pos + new Vector3(x * 0.1f, y * 0.07f, 0), transform.TransformDirection(Vector3.forward) * minDistanceFromAttachment, Color.yellow);
+
+                if (Physics.Raycast(ray, out hit, minDistanceFromAttachment, attachments) )
+                {
+                    currentobject = hit.rigidbody.gameObject;
+                    currentState = 1;
+                    AmountHit++;
+                }
+                
+            }
         }
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * minDistanceFromAttachment, Color.yellow);
+        if (AmountHit == 0) { currentState = default; }
+    }
+    public void state()
+    {
+        switch (currentState)
+        {
+            case 1:
+                if (currentobject.GetComponent<AttachmentStats>() != null)
+                {
+                    getStats();
+                    showStats();
+                    attachmentStatsUi.GetComponent<Canvas>().enabled = true;
+                }
+                break;
+
+
+            default:
+                attachmentStatsUi.GetComponent<Canvas>().enabled = false;
+                break;
+        }
+    }
+    public void getStats()
+    {
+
+        
+            statList = currentobject.GetComponent<AttachmentStats>().statList;
+            sprite.GetComponent<Image>().sprite = currentobject.GetComponent<AttachmentUIPart>().getImage();
+            spread = statList[0];
+            amountOfBullets = statList[1];
+            damage = statList[2];
+            range = Mathf.Abs(statList[3] * statList[4]);
+        
+       
+    }
+    public void showStats()
+    {
+        TstatList[0].GetComponent<Text>().text = "Spread = " + spread;
+        TstatList[1].GetComponent<Text>().text = "Bullets = " + amountOfBullets;
+        TstatList[2].GetComponent<Text>().text = "Damage = " + damage;
+        TstatList[3].GetComponent<Text>().text = "Range = " + range;
+        for (int i = 5; i < statList.Count; i++)
+        {
+            switch (i - 5)
+            {
+                case 0:
+                    if (statList[5] == 1) { setText.GetComponent<Text>().text = "Minigun"; }
+                    break;
+                case 1:
+                    if (statList[6] == 1) { setText.GetComponent<Text>().text = "Grenade"; }
+                    break;
+                case 2:
+                    if (statList[7] == 1) { setText.GetComponent<Text>().text = "Shotgun"; }
+                    break;
+                case 3:
+                    if (statList[8] == 1) { setText.GetComponent<Text>().text = "Sniper"; }
+                    break;
+            }
+        }
     }
 }
