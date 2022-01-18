4904b2f854118c7dc09618e29e9e9d18fa580b04
hsuwandhy <hsuwandhy@gmail.com>
2022-01-18 16:27:37 +0100

    Lava script

--- PGD-GAM-JAM-2/Assets/Scripts/Map/LavaHazzardScript.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Map/LavaHazzardScript.cs
@@ -4,24 +4,21 @@ using UnityEngine;
 
 public class LavaHazzardScript : MonoBehaviour
 {
+    private float lavaTimer=0;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            //add what happens if the player enters lava here
-            //Sound
-            collision.gameObject.GetComponent<PlayerHealthScript>().takeDamage(3);
+            lavaTimer += Time.deltaTime;
+            if (lavaTimer < 1)
+            {
+                //add what happens if the player enters lava here
+                //Burn Sound
+                collision.gameObject.GetComponent<PlayerHealthScript>().takeDamage(1);
+            }
+            if (lavaTimer > 3) { lavaTimer = 0; }
         }
     }
 }
