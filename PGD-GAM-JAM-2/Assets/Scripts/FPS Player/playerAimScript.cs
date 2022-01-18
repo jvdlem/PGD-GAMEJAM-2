af9d4ea303ac698d7d5d7c470960cd338fc0f478
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2022-01-18 23:42:25 +0100

    fucking reload animation fucking awesome

--- PGD-GAM-JAM-2/Assets/Scripts/FPS Player/playerAimScript.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/FPS Player/playerAimScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class playerAimScript : MonoBehaviour
 {
     public Animator anim;
-    public bool aiming;
+    public bool aiming, reloading;
+    public float reloadTimer = 1;
 
     // Update is called once per frame
     void Update()
@@ -20,5 +21,27 @@ public class playerAimScript : MonoBehaviour
             anim.SetBool("aiming", false);
             aiming = false;
         }
+
+        if (reloadTimer >= 1 && Input.GetKeyDown("r"))
+        {
+            reloading = true;
+        }
+
+        if (reloading)
+        {
+            reloadTimer -= Time.deltaTime;
+            anim.SetBool("aiming", false);
+            aiming = false;
+            anim.SetBool("reloading", true);
+        }
+
+        if (reloadTimer <= 0)
+        {
+            anim.SetBool("reloading", false);
+            reloading = false;
+            reloadTimer = 1;
+        }
+
+
     }
 }
