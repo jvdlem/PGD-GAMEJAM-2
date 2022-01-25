bcf34e82b171f798b4d5dd5afc086e62ba8681a8
nijmanc2 <90317886+DeCeiman@users.noreply.github.com>
2022-01-25 17:24:56 +0100

    Added M&T scene

--- PGD-GAM-JAM-2/Assets/Scripts/Shop/EnterShop.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Shop/EnterShop.cs
@@ -5,10 +5,12 @@ using UnityEngine.Events;
 
 public class EnterShop : MonoBehaviour
 {
+    //Get Shop script and spawners
     [SerializeField] ShopScript shopScript;
     [SerializeField] List<GameObject> itemSpawners = new List<GameObject>();
     public void OnTriggerEnter(Collider other)
     {
+        //When player enters the shop spawn in items and destroy the trigger
         if (other.gameObject.tag == "Player")
         {
             Destroy(this.gameObject);
