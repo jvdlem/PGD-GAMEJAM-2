002e294880ec0e725a786b8bca4c6933b968e2b2
Terdirk12 <71409021+Terdirk12@users.noreply.github.com>
2022-01-27 15:38:24 +0100

    bugfixes
    -no more reloading while reloading
    -no more shooting while reloading
    -no more crosshair while aiming

--- PGD-GAM-JAM-2/Assets/Scripts/FPS Player/playerAimScript.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/FPS Player/playerAimScript.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class playerAimScript : MonoBehaviour
 {
+    public GameObject crosshair;
     public Animator anim;
-    public bool aiming, reloading;
-    public float reloadTimer = 1;
+    public bool aiming;
+    public static bool isAiming;
+    public static float reloadTimer = 1;
 
     // Update is called once per frame
     void Update()
@@ -15,34 +17,32 @@ public class playerAimScript : MonoBehaviour
         {
             anim.SetBool("aiming", true);
             aiming = true;
+            isAiming = true;
         }
         else if (Input.GetButtonDown("Fire2") && aiming)
         {
             anim.SetBool("aiming", false);
             aiming = false;
+            isAiming = false;
         }
 
-        if (reloadTimer >= 1 && Input.GetKeyDown("r"))
+        if (Pistol.reloading)
         {
-            reloading = true;
-        }
-
-        if (reloading)
-        {
+            if (reloadTimer <= 0.2f) FMODUnity.RuntimeManager.PlayOneShot("event:/Gun/Reload");
             reloadTimer -= Time.deltaTime;
-            anim.SetBool("aiming", false);
             aiming = false;
+            anim.SetBool("aiming", false);
             anim.SetBool("reloading", true);
         }
-
-        if(reloadTimer <= 0.2f) FMODUnity.RuntimeManager.PlayOneShot("event:/Gun/Reload");
-
-        if (reloadTimer <= 0)
+        else if (reloadTimer <= 0)
         {
             anim.SetBool("reloading", false);
-            reloading = false;
             reloadTimer = 1;
         }
+
+
+
+
 
 
     }
