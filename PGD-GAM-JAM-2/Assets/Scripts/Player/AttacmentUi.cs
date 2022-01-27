61dac3305f886a28c62649dab6ce6ce59d46781e
EslamiAA <90322106+EslamiAA@users.noreply.github.com>
2022-01-27 12:20:14 +0100

    Teleport are added

--- PGD-GAM-JAM-2/Assets/Scripts/Player/AttacmentUi.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Player/AttacmentUi.cs
@@ -67,7 +67,7 @@ public class AttacmentUi : MonoBehaviour
         switch (currentState)
         {
             case 1:
-                if (currentobject.GetComponent<AttachmentStats>() != null)
+                if (currentobject.GetComponent<AttachmentStats>() != null && !aimingScript.aiming && gun != null && gun.GetComponent<Pistol>().isInMenu == false)
                 {
                     getStats();
                     showStats();
