2b7c2e5fb98e690fe8df2d46c70f67dcace4bc09
JordyWolf <33620374+Jordywolf@users.noreply.github.com>
2022-02-03 17:16:06 +0100

    boss works with sniper

--- PGD-GAM-JAM-2/Assets/Scripts/Bosses/EyeBossScript.cs
+++ PGD-GAM-JAM-2/Assets/Scripts/Bosses/EyeBossScript.cs
@@ -69,4 +69,34 @@ public class EyeBossScript : EnemyBaseScript
             }
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check if eye that gets hit is active
+        if (EyeIsActive)
+        {
+            if (other.gameObject.tag == "Projectile")
+            {
+                //Check if projectile that hit has the "Projectille" base class
+                if (other.gameObject.GetComponent<Projectille>())
+                {
+                    //Deal x amount of damage to the boss parent, x being the damage the projectile does
+                    parentBoss.BossCurrentHealth -= (int)other.gameObject.GetComponent<Projectille>().dmg;
+
+                    //Check if boss goes under the next health trigger, if yes set bool CycleToNextEye in parent boss scipt to true
+                    if (parentBoss.BossCurrentHealth <= parentBoss.NextHealthTrigger)
+                    {
+                        parentBoss.BossCurrentHealth = parentBoss.NextHealthTrigger;
+                        parentBoss.CycleToNextEye = true;
+                    }
+
+                    //If the boss hits 0 HP or goes under, go to the Die State
+                    if (parentBoss.BossCurrentHealth <= 0)
+                    {
+                        parentBoss.CurrentBossState = BossScript.BossStates.DieState;
+                    }
+                }
+            }
+        }
+    }
 }
